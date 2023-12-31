6d310c9fb830ac2cf618a5187c0530c4369c0c11
aymantarek16 <aymantarek16@yahoo.com>
2023-12-31 03:27:36 +0200

    add helmet package

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,12 +3,18 @@ import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
 import { store } from './Redux/store';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-    <App />
+    <HelmetProvider>
+    <Helmet>
+        <title>Ecommerce</title>
+      </Helmet>
+      <App />
+    </HelmetProvider>
   </Provider>
 );
 
--- frontend/src/pages/details/Product-details.jsx
+++ frontend/src/pages/details/Product-details.jsx
@@ -2,17 +2,28 @@ import React, { useRef, useState } from "react";
 import "./product-details.css";
 import { useGetOneProductQuery } from "../../Redux/productsApi";
 import { useParams } from "react-router-dom";
-import { Badge, Box, Button, CircularProgress, IconButton, Typography } from "@mui/material";
+import {
+  Badge,
+  Box,
+  Button,
+  CircularProgress,
+  IconButton,
+  Typography,
+} from "@mui/material";
 import DetailsThumb from "./DetailsThumb";
 import styled from "@emotion/styled";
 import { useDispatch, useSelector } from "react-redux";
-import { addToCart, decreaseQuantity, increaseQuantity } from "../../Redux/cartSlice";
+import {
+  addToCart,
+  decreaseQuantity,
+  increaseQuantity,
+} from "../../Redux/cartSlice";
 import { Add, Remove, ShoppingCart } from "@mui/icons-material";
+import { Helmet } from "react-helmet-async";
 
 const StyledBadge = styled(Badge)(({ theme }) => ({
   "& .MuiBadge-badge": {},
 }));
-
 
 const ProductDetails = () => {
   const dispatch = useDispatch();
@@ -28,7 +39,6 @@ const ProductDetails = () => {
 
     return myProduct.quantity;
   };
-
 
   let { id } = useParams();
   // data => only one product
@@ -68,6 +78,9 @@ const ProductDetails = () => {
   if (data) {
     return (
       <div className="app details-page">
+        <Helmet>
+          <title>{`ProductDetails -${data.id}`}</title>
+        </Helmet>
         <div className="details">
           <div className="big-img">
             <img src={data.imageLink[index]} alt="" />
@@ -87,59 +100,58 @@ const ProductDetails = () => {
               tab={handleTab}
               myRef={myRef}
             />
-                {selectedProductsID.includes(data.id) ? (
-                  <div
-                    dir="ltr"
-                    style={{
-                      display: "flex",
-                      alignItems: "center",
-                      marginTop: "30px",
-                    }}
-                  >
-                    <IconButton
-                      color="primary"
-                      sx={{ mr: "15px" }}
-                      onClick={() => {
-                        dispatch(decreaseQuantity(data));
-                      }}
-                    >
-                      <Remove fontSize="small" />
-                    </IconButton>
+            {selectedProductsID.includes(data.id) ? (
+              <div
+                dir="ltr"
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  marginTop: "30px",
+                }}
+              >
+                <IconButton
+                  color="primary"
+                  sx={{ mr: "15px" }}
+                  onClick={() => {
+                    dispatch(decreaseQuantity(data));
+                  }}
+                >
+                  <Remove fontSize="small" />
+                </IconButton>
 
-                    <StyledBadge
-                      badgeContent={productQuantity(data)}
-                      color="primary"
-                    />
+                <StyledBadge
+                  badgeContent={productQuantity(data)}
+                  color="primary"
+                />
 
-                    <IconButton
-                      color="primary"
-                      sx={{ ml: "15px" }}
-                      onClick={() => {
-                        dispatch(increaseQuantity(data));
-                      }}
-                    >
-                      <Add fontSize="small" />
-                    </IconButton>
-                  </div>
-                ) : (
-                  <Button
-                    sx={{
-                      textTransform: "capitalize",
-                      p: 1,
-                      lineHeight: 1.1,
-                      mt : 3
-                    }}
-                    variant="contained"
-                    color="primary"
-                    onClick={() => {
-                      dispatch(addToCart(data));
-                    }}
-                  >
-                     <ShoppingCart sx={{ fontSize: "18px", mr: 1 }} />
-                    Add to cart
-                  </Button>
-                )}
-
+                <IconButton
+                  color="primary"
+                  sx={{ ml: "15px" }}
+                  onClick={() => {
+                    dispatch(increaseQuantity(data));
+                  }}
+                >
+                  <Add fontSize="small" />
+                </IconButton>
+              </div>
+            ) : (
+              <Button
+                sx={{
+                  textTransform: "capitalize",
+                  p: 1,
+                  lineHeight: 1.1,
+                  mt: 3,
+                }}
+                variant="contained"
+                color="primary"
+                onClick={() => {
+                  dispatch(addToCart(data));
+                }}
+              >
+                <ShoppingCart sx={{ fontSize: "18px", mr: 1 }} />
+                Add to cart
+              </Button>
+            )}
           </div>
         </div>
       </div>
